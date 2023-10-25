1313f0ef528c0044b781ccebb68f2bb4276114d8
darkPigeonn <nrio2864@gmail.com>
2023-10-25 09:24:09 +0700

    update icon pse

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
     ],
 
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/pse.ico" },
       {
         rel: "stylesheet",
         href: "/css/bootstrap.min.css",
